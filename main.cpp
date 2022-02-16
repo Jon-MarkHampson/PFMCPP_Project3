66ff006a89973b6aad54c9aa22b4e9c3d1ba27c1
Jon-Mark Hampson <33136955+Jon-MarkHampson@users.noreply.github.com>
2022-02-16 00:16:07 +0000

    turned pseudo code to code

--- main.cpp
+++ main.cpp
@@ -198,7 +198,47 @@ Thing 1) Coffee Shop
     1) host local artists work on walls
     2) charge customer
     3) train staff
+*/
 
+struct CoffeeShop
+{
+
+    //number of chairs (int)
+    int numChairs = 54;
+    //number of staff (int)
+    int numStaff = 7;
+    //number of toilets (int)
+    int numToilets = 2;
+    //annual profit (double)
+    double annualProfit = 45362.78;
+    //price of coffee (float)
+    float priceCoffee = 3.40f;
+
+    struct Employee
+    {
+        std::string firstName = "Magnus";
+        std::string lastName = "Magnusson";
+        int employeeID = 12345;
+        bool fullyTrained = true;
+        float salary = 30000.00f;
+
+        void makeDeliveries();
+        void takeHoliday();
+        bool deservesPromotion(); // returns whether the employee is derving of a promotion
+
+
+
+    };
+
+    //host local artists work on walls
+    float getArtExhibitCommision(float percenatgeCommisionAgreed, float totalArtSales, CoffeeShop coffeeshop); //returns the total value of commission earn from the sale of all the art at this instance of CoffeeShop 
+    //charge customer
+    float getCustomerSale(float priceCoffee, int numberOfCoffees, int customerNumber); //returns the total value of the sale of all good for this customer
+    //train staff
+    void staffTraining(Employee employee, std::string trainingRequired);
+};
+
+/*
 Thing 2) School
 5 properties:
     1) number of classrooms (int)
@@ -210,7 +250,30 @@ Thing 2) School
     1) train teachers
     2) students christmas play
     3) close for holidays
+*/
 
+struct School
+{
+    //number of classrooms (int)
+    int numClassrooms = 111;
+    //position in leauge table (int)
+    int leagueTablePosition = 13;
+    ///number of teachers (int)
+    int numTeachers = 54;
+    //number of pupils (int)
+    int numPupils = 1111;
+    //student average grade (float)
+    float aveGrade = 3.45f;
+
+    //train teachers
+    void teacherTraining(bool internalTraining, std::string trainingRequired);
+    //students christmas play
+    void christmasPlay(int numberOfStudentsInCast, std::string nameOfPlay, int rehearsalDays);
+    //close for holidays
+    void holidayClosure(int howManyDaysClosed, std::string nameOfHoliday);
+};
+
+/*
 Thing 3) Farm
 5 properties:
     1) number of sheep (int)
@@ -222,7 +285,30 @@ Thing 3) Farm
     1) sell sheep
     2) harvest crop
     3) construct new barn
+*/
 
+struct Farm
+{
+    //number of sheep (int)
+    int numSheep = 1234;
+    //number of cows (int)
+    int numCows = 321;
+    //crop yeild (double)
+    double cropYield = 76543.21;
+    //number of farmers (int)
+    int numFarmers = 6;
+    //area in hectare (double)
+    double farmLandAreaInHectare = 84.3;
+
+    //sell sheep
+    float getTurnoverSheepSale(float priceOfSheep, int numSheep); //returns the income from the sale of this farms sheep
+    //harvest crop
+    void harvestCrop(bool isCropMature, int numFarmHands, bool machinesFueled);
+    //construct new barn
+    void constructNewBarn(bool needNewBarn, float newConstructionBudget);
+};
+
+/*
 Thing 4) Commerical Aeroplane
 5 properties:
     1) number of engines (int)
@@ -234,7 +320,44 @@ Thing 4) Commerical Aeroplane
     1) take off
     2) emergency landing
     3) inflight entertainment
+*/
 
+struct CommericalAeroplane
+{
+    //number of engines (int)
+    int numEngines = 4;
+    //top speed (double)
+    double maxAirSpeedMPH = 576.43;
+    //number of seats (int)
+    int numSeats = 524;
+    //fuel tank capacity (double)
+    double fuelTankCapacityInLitres = 238840.04;
+    //flight range (double)
+    double flightRangeInKM = 14310.23;
+
+    struct Cockpit
+    {
+        float age = 1.35f;
+        int numSeats = 5;
+        int numWindows = 4;
+        bool hasBlackBox = true;
+        bool ElectronicFlightInstrumentSystem = true;
+
+        void increaseAirSpeed(float deltaV, float currentAirSpeed);
+        void groundProximityWarningAlarm(bool nearGround, float proximityWarningThreshold);
+        void autopilotDisengagement(bool pilotAndCopilotChecklistComplete);
+    };
+
+
+    //take off
+    bool takeOffComplete(float groundSpeed, bool landingGear, bool armDoorsAndCrossCheck); //returns whether has this instance of Commercial Areoplane completed a successful take off
+    //emergency landing
+    void emergencyLanding(bool enginefailure, float urrentAirSpeed);
+    //inflight entertainment
+    void inflightEntertainment(int passengerSeatNumber, std::string movieSelction);
+};
+
+/*
 Thing 5 ) Player
 5 properties:
     1) body weight (float)
@@ -246,7 +369,30 @@ Thing 5 ) Player
     1) run
     2) score
     3) train
+*/
 
+struct Player
+{
+    //body weight (float)
+    float bodyMassKG = 101.34f;
+    //sponsor (std:string)
+    std::string sponsor = "Nike";
+    //number of games played (int)
+    int numGamesPlayed = 104;
+    //scoring average (float)
+    float scoringAverage = 1.78f;
+    //name (std::string)
+    std::string name = "Ronaldo";
+
+    //run
+    float getDistanceRun(int howFast, float howFar, bool startWithLeftFoot);
+    //score
+    void score(bool hasBall, bool inScoringPosition);
+    //train
+    void train(std::string trainingRequired, float fitnessScore);
+};
+
+/*
 Thing 6) Coach
 5 properties:
     1) name (std::string)
@@ -258,19 +404,65 @@ Thing 6) Coach
     1) give motivational speech
     2) lose job
     3) give press conference
+*/
 
+struct Coach
+{
+    //name (std::string)
+    std::string name;
+    //years experience (int)
+    int yearsExperience = 4;
+    //trophies won (int)
+    int numTrophiesWon = 2;
+    //salary (double)
+    double salary = 8078900.99;
+    //inspiring (bool)
+    bool inspiringn = false;
+
+    //give motivational speech
+    void deliverMotivationalSpeech(bool winningStreak);
+    //lose job
+    void getSacked(int numberOfGamesInLosingStreak);
+    //give press conference
+    void givePressConference(bool wonGame);
+};
+
+/*
 Thing 7) Fan
 5 properties:
     1) name (std::string)
     2) age (int)
     3) season ticket holder (bool)
     4) distance from stadium (double)
-    5) fan club member (bool)
+    5) fanclub member (bool)
 3 things it can do:
     1) buy ticket to game
     2) cheer
     3) wear team colours
+*/
 
+struct Fan
+{
+    //name (std::string)
+    std::string name = "John Johnson";
+    //age (int)
+    int age;
+    //season ticket holder (bool)
+    bool hasSeasonTicket = true;
+    //distance from stadium (double)
+    double distanceFromStadiumInKM = 345.78;
+    //fanclub member (bool)
+    bool fanclubMember = true;
+
+    //buy ticket to game
+    void purchseTicket(float ticketPrice, std::string opponentTeam);
+    //cheer
+    void cheer(bool teamScoredGoal);
+    //wear team colours
+    void wearTeamColours(bool teamWinningStreak);
+};
+
+/*
 Thing 8) Medic
 5 properties:
     1) qualifications (std::string)
@@ -282,19 +474,65 @@ Thing 8) Medic
     1) give steriod injection
     2) massage treatment
     3) make fitness assessment
+*/
 
+struct Medic
+{
+    //qualifications (std::string)
+    std::string qualifications;
+    //name (std::string)
+    std::string name = "The FA Level 4 Emergency Medical Management in Football";
+    //salary (double)
+    double salary = 178909.99;
+    //explayer (bool)
+    bool isExPlayer = true;
+    //IT skills (bool)
+    bool hasITSkills = true;
+
+    //give steriod injection
+    void giveSteriodInjection(Player player, std::string bodyPart, bool servePain);
+    //massage treatment
+    void giveMassageTreatment(Player player, std::string therapyTargetArea);
+    //make fitness assessment
+    float getFitnessAssessmentScore(int restingBPM, int maxBPM, float bloodPressure); //returns value of a players fitness assessment
+};
+
+/*
 Thing 9) Stadium
 5 properties:
     1) number of seats (int)
     2) pitch area (float)
-    3) toilets (int)
+    3) number of toilets (int)
     4) training pitch (bool)
     5) construction cost (double)
 3 things it can do:
     1) host home game
     2) covid vaccination centre
     3) require maintance
+*/
 
+struct Stadium
+{
+    //number of seats (int)
+    int numSeats = 62850;
+    //pitch area (float)
+    float pitchAreaInSqMetres = 7123.89f;
+    //number of toilets (int)
+    int numToilets = 66;
+    //training pitch (bool)
+    bool trainingPitch = true;
+    //construction cost (double)
+    double constructionCost = 1200500700.99;
+
+    //host home game
+    void homeGame(std::string nameVistingTeam);
+    //covid vaccination centre
+    bool covidVaccinationCentre(int numberOfUnvaccinated, float RNumber); //returns wheteher stadium is required to be vaccination centre
+    //require maintance
+    void maintance(bool brokenToilet);
+};
+
+/*
 Thing 10) Sports Team
 5 properties:
     1) player
@@ -307,6 +545,37 @@ Thing 10) Sports Team
     2) play away game
     3) win championship
 */
+struct TeamCaptain //to prevent compile errors
+{
+    std::string name = "David Davison";
+};
+struct NumberOneFan //to prevent compile errors
+{
+    std::string name = "William Williamson";
+};
+
+
+struct SportsTeam
+{
+    //player
+    Player player;
+    //coach
+    Coach coach;
+    //fan
+    NumberOneFan numberOneFan;
+    //medic
+    TeamCaptain TeamCaptain;
+    //stadium
+    Stadium stadium;
+
+    //play home game
+    bool winHomeGame(int goalsScoredFor, int goalsScoredAgainst); //returns whether the team have won the home game
+    //play away game
+    bool winAwayGame(int goalsScoredFor, int goalsScoredAgainst); //returns whether the team have won the away game
+    //win championship
+    bool winChampionship(int gamesPlayed, int gamesWon, int gamesLost, int gamesDrawn, int totalGoalsScoredFor, int totalGoalsScoredAgainst); //returns whether the team have won the championship
+};
+
 
 #include <iostream>
 int main()
